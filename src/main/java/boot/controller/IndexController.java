431fb5352b060f0361580f4070b59bfdad0ea17b
Aivar Magi <aivar.magi@tieto.com>
2015-07-24 11:00:38 +0300

    securityconf lisatud, index template lisatud

---
+++ src/main/java/boot/controller/IndexController.java
@@ -0,0 +1,13 @@
+package boot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping("/")
+	String index(){
+		return "index";
+	}
+}
